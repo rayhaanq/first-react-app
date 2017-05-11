67515bd6afea4b834bb2661c1da5215379d6f604
Rayhaan Quazi <rayhaan.quazi@gmail.com>
2017-05-12 00:42:18 +0100

    added properties with defaults

--- public/app.jsx
+++ public/app.jsx
@@ -1,11 +1,23 @@
 let Greeter = React.createClass({
+
+  getDefaultProps: function (){
+    return {
+
+      name: 'React',
+      msg: 'This is a component'
+    };
+  },
+
+
   render: function() {
+    let name = this.props.name;
+    let msg = this.props.msg;
     return (
 
       <div>
-        
-        <h1>Hello React!</h1>
-        <p>This is a component</p>
+
+        <h1>Hello {name}!</h1>
+        <p>{msg}</p>
 
       </div>
 
@@ -14,7 +26,9 @@ let Greeter = React.createClass({
   }
 });
 
+let firstName = "Rayhaan";
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
